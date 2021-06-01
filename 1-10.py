1ec5de24e87540dbe5585c255fe752acb5d5345a
djangoapp1- <jastynmanya@gmail.com>
2021-06-01 13:56:55 +0300

    finally i can make it

---
+++ 1-10.py
@@ -0,0 +1,10 @@
+#write a program to print all prime numbers from 1-10
+
+
+for x in range(1, 10):
+    if x > 1:
+        for i in range(2,x):
+            if(x%i==0):
+                break
+        else:
+            print(x)
